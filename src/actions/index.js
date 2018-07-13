76e4f300ce29c68058723d58084e578436c49c92
Isil Donmez <isil.donmez@gmail.com>
2018-07-13 13:48:49 +0100

    Improve loginUser in  actioncreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase';
 import {
 	EMAIL_CHANGED,
-	PASSWORD_CHANGED
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => {
@@ -19,11 +21,27 @@ export const passwordChanged = (text) => {
 };
 
 // by using dispatch below we can do whatever async actions we want.
+// Because we have direct access to dispatch method now,
+// we can dispatch as many actions as we'd like from a single action creator.
 export const loginUser = ({ email, password }) => {
 	return (dispatch) => {
 		firebase.auth().signInWithEmailAndPassword(email, password)
-			.then(user => {
-				dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+			.then(user => loginUserSuccess(dispatch, user))
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(user => loginUserSuccess(dispatch, user))
+					.catch(() => loginUserFail(dispatch));
 			});
-		};
+	};
+};
+
+const loginUserFail = (dispatch) => {
+	dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	});
 };
